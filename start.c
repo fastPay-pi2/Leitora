24917be1fed3547f4ed9be828a2582e5d18f809a
Vitorsulzbach <vjsulzbach@hotmail.com>
2019-10-16 13:34:41 -0300

    adicionado demultiplexação de antenas, LOG está funcionando mas perdeu a organização, código também não organizado

--- start.c
+++ start.c
@@ -12,6 +12,7 @@ FILE *log;
 struct timeval  tv1, tv2, tv3, tv4, tv5, tv6, tv7, tv8;
 bool debug = true;
 int count=1, n_cycles = 3;
+int8_t n_antenas = 2;
 
 typedef struct reader_s {
 	tag1_t tag_scan1;
@@ -45,8 +46,9 @@ typedef struct list_tag1 {
 
 void print_tag1(struct tag1 *h);
 bool cmp_tag1(struct tag1 a, struct tag1 b);
-struct list_e_tag1* catch_tags(int n_cycles);
-void init();
+struct list_e_tag1* catch_tags(struct list_e_tag1 *list, int n_cycles);
+void init_reader();
+void init_reader_config();
 static void activate(GtkApplication* app, gpointer user_data);
 void add_e_tag1(struct list_e_tag1 *list, struct tag1 tag);
 void print_e_list(struct list_e_tag1 *list);
@@ -55,8 +57,9 @@ void add_li_e_unrepeated(struct list_e_tag1 *list, struct tag1 c);
 void del_e_li(struct list_e_tag1 *list);
 void open_log();
 int32_t handle_rfid_module_read2();
-struct list_e_tag1* catch_tags_debug(int n_cycles);
+struct list_e_tag1* catch_tags_debug(struct list_e_tag1 *list, int n_cycles);
 void cycle();
+void set_antenna(uint8_t arg);
 
 //-------------------------------------------------------------------------------------- INÍCIO MAIN --------------------------------------------------------------------------------------//
 //-------------------------------------------------------------------------------------- INÍCIO MAIN --------------------------------------------------------------------------------------//
@@ -66,9 +69,15 @@ int main(int argc, char **argv){
 	if(debug){
 		log = fopen("log.txt","w+");
 		fprintf(log, "Log de Temporização:\n");
+		gettimeofday(&tv7, NULL);
+	}
+	init_reader_config();
+	init_reader();
+	if(debug){
+		gettimeofday(&tv8, NULL);
+		fprintf(log, "Tempo de init_reader() && init_reader_config(): %f s\n",(double)(tv8.tv_usec - tv7.tv_usec)/1000000 + (double)(tv8.tv_sec - tv7.tv_sec));
 		fclose(log);
 	}
-	init();
 	//~ GtkApplication *app;
 	//~ int status;
 	//~ app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
@@ -76,8 +85,33 @@ int main(int argc, char **argv){
 	//~ status = g_application_run(G_APPLICATION (app), argc, argv);
 	//~ g_object_unref(app);
 	while(1){
-		cycle();
-		printf("\nDigite para fazer um novo ciclo:\n");
+		struct list_e_tag1 *list = malloc(sizeof(struct list_tag1));
+		if(debug)
+				gettimeofday(&tv7, NULL);
+		for(uint8_t a=0;a<n_antenas;a++){
+			if(debug){
+				open_log();
+				fprintf(log, "\n\nCiclo %d.",count);
+				fclose(log);
+				list = catch_tags_debug(list, n_cycles);
+			} else {
+				list = catch_tags(list, n_cycles);
+			}
+			set_antenna(a+1);
+		}
+		if(debug)
+				gettimeofday(&tv8, NULL);
+		system("clear");
+		printf("ciclo %d:\n\n",count++);
+		print_e_list(list);
+		if(debug){
+			open_log();
+			fprintf(log, "\n\nTempo do ciclo: %f\n\n-----------------------------------------------------------\n-----------------------------------------------------------\n-----------------------------------------------------------\n\n\n",(double)(tv8.tv_usec - tv7.tv_usec)/1000000 + (double)(tv8.tv_sec - tv7.tv_sec));
+			fclose(log);
+		}
+		del_e_li(list);
+		free(list);
+		printf("\nDigite 's' para fazer um novo ciclo:\n");
 		while(getc(stdin)=='\n');
 		//sleep(2);
 	}
@@ -88,6 +122,19 @@ int main(int argc, char **argv){
 //-------------------------------------------------------------------------------------- FIM MAIN --------------------------------------------------------------------------------------//
 //-------------------------------------------------------------------------------------- FIM MAIN --------------------------------------------------------------------------------------//
 
+void set_antenna(uint8_t arg){
+	if((arg>0)||(arg<9)){
+		int32_t val = SetAntenna(&r.r2, arg);
+		if (val < 0){
+			printf("\noperation failed! (%d)\n", val);
+			Gen2ReaderDestroy(&r.r2);
+			init_reader_config();
+		}
+	} else{
+		printf("\nargumento errado para o set_antenna\n");
+	}
+}
+
 void open_log(){
 	log = fopen("log.txt","r+");
 	if(log==NULL){
@@ -97,33 +144,8 @@ void open_log(){
 	fseek(log, 0, SEEK_END);
 }
 
-void cycle(){
-	struct list_e_tag1 *list;
-	if(debug){
-		open_log();
-		fprintf(log, "\n\nCiclo %d.",count);
-		fclose(log);
-		gettimeofday(&tv7, NULL);
-		list = catch_tags_debug(n_cycles);
-	} else {
-		list = catch_tags(n_cycles);
-	}
-	if(debug)
-			gettimeofday(&tv8, NULL);
-	system("clear");
-	printf("ciclo %d:\n\n",count++);
-	print_e_list(list);
-	if(debug){
-		open_log();
-		fprintf(log, "\n\nTempo do ciclo: %f\n\n-----------------------------------------------------------\n-----------------------------------------------------------\n-----------------------------------------------------------\n\n\n",(double)(tv8.tv_usec - tv7.tv_usec)/1000000 + (double)(tv8.tv_sec - tv7.tv_sec));
-		fclose(log);
-	}
-	del_e_li(list);
-	free(list);
-}
 
-struct list_e_tag1* catch_tags(int n_cycles){ //Função para captura de CÓDIGOS de Tags, retorna uma lista encadeada de TAGS não repetidas, a quantidade de cilos determinar quantas vezes a leitora ira fazer um pacote de TAGS para aumentar a chance de todas as TAGS serem lidas
-	struct list_e_tag1 *list = malloc(sizeof(struct list_tag1));
+struct list_e_tag1* catch_tags(struct list_e_tag1 *list, int n_cycles){ //Função para captura de CÓDIGOS de Tags, retorna uma lista encadeada de TAGS não repetidas, a quantidade de cilos determinar quantas vezes a leitora ira fazer um pacote de TAGS para aumentar a chance de todas as TAGS serem lidas
 	int te = 1;
 	list->head=NULL;
 	list->size = 0;
@@ -145,8 +167,7 @@ struct list_e_tag1* catch_tags(int n_cycles){ //Função para captura de CÓDIGO
 	return list;
 }
 
-struct list_e_tag1* catch_tags_debug(int n_cycles){ //Função para captura de CÓDIGOS de Tags, retorna uma lista encadeada de TAGS não repetidas, a quantidade de cilos determinar quantas vezes a leitora ira fazer um pacote de TAGS para aumentar a chance de todas as TAGS serem lidas
-	struct list_e_tag1 *list = malloc(sizeof(struct list_tag1));
+struct list_e_tag1* catch_tags_debug(struct list_e_tag1 *list, int n_cycles){ //Função com debug
 	open_log();
 	int te;
 	list->head=NULL;
@@ -172,11 +193,10 @@ struct list_e_tag1* catch_tags_debug(int n_cycles){ //Função para captura de C
 				}
 				gettimeofday(&tv2, NULL);
 				if(te>9){
-					fprintf(log,"Tempo para adicionar %dª tag: %f s\n", te, (double)(tv2.tv_usec - tv1.tv_usec)/1000000 + (double)(tv2.tv_sec - tv1.tv_sec));
+					fprintf(log,"Tempo para adicionar %dª tag: %f s\n", te++, (double)(tv2.tv_usec - tv1.tv_usec)/1000000 + (double)(tv2.tv_sec - tv1.tv_sec));
 				} else {
-					fprintf(log,"Tempo para adicionar 0%dª tag: %f s\n", te, (double)(tv2.tv_usec - tv1.tv_usec)/1000000 + (double)(tv2.tv_sec - tv1.tv_sec));
+					fprintf(log,"Tempo para adicionar 0%dª tag: %f s\n", te++, (double)(tv2.tv_usec - tv1.tv_usec)/1000000 + (double)(tv2.tv_sec - tv1.tv_sec));
 				}
-				te++;
 			}
 			gettimeofday(&tv4, NULL);
 			gettimeofday(&tv6, NULL);
@@ -201,11 +221,7 @@ int32_t handle_rfid_module_read2(){
 	if (val < 0){
 		printf("\noperation failed! (%d)\n", val);
 		Gen2ReaderDestroy(&r.r1);
-		val = Gen2ReaderCreate("192.168.5.10", &r.r1);
-		if (val){
-			printf("Error creating reader session.\n");
-			exit(-1);
-		}
+		init_reader();
 	} else if(r.response.code == MSG_TYPE_ERROR){
 		printf("\nError on RFID module: %02x %02x %02x %02x", r.response.code, r.response.payload[0], r.response.payload[1], r.response.payload[2]);
 	}
@@ -261,8 +277,21 @@ bool cmp_li_e_tag1(struct list_e_tag1 *list, struct tag1 c){
 	return true;
 }
 
-void init(){ //inicia o socket???(a definir)
+void init_reader(){ //inicia o socket???(a definir)
+	//~ Gen2ReaderDestroy(&r.r2);
+	//~ Gen2ReaderDestroy(&r.r1);
 	int32_t val = Gen2ReaderCreate("192.168.5.10", &r.r1);
+	if (val)
+	{
+		printf("Error creating reader session.\n");
+		exit(1);
+	}
+}
+
+void init_reader_config(){
+	//~ Gen2ReaderDestroy(&r.r2);
+	//~ Gen2ReaderDestroy(&r.r1);
+	int32_t val = Gen2ReaderCreate("192.168.5.10", &r.r2);
 	if (val)
 	{
 		printf("Error creating reader session.\n");
